761334b7c76a6f9e77bba8e73a55df1161d3aa34
Ostap <mamchurostap26@gmail.com>
2023-05-25 18:37:38 +0300

    update error messages

--- src/features/user/create-user/model/userFormSchema.js
+++ src/features/user/create-user/model/userFormSchema.js
@@ -1,17 +1,14 @@
 import * as yup from "yup";
 
 const userFormSchema = yup.object({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  email: yup
-    .string()
-    .required()
-    .email(),
-  phoneNumber: yup.string().required(),
+  firstName: yup.string().required("First name is required"),
+  lastName: yup.string().required("Last name is required"),
+  email: yup.string().required("Email is required").email("Email is invalid"),
+  phoneNumber: yup.string().required("Phone number is required"),
   dateOfBirth: yup
     .date()
-    .min(new Date('1900-01-01'))
-    .max(new Date())
-    .required()
+    .min(new Date("1900-01-01"), "Date of birth is invalid")
+    .max(new Date(), "Date of birth is invalid")
+    .required("Date of birth is required"),
 });
-export { userFormSchema }
+export { userFormSchema };
