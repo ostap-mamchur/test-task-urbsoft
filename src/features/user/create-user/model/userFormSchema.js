1b4cf620cd3e4e8afb46bb3aade75d257135bd64
Ostap <mamchurostap26@gmail.com>
2023-05-26 11:17:48 +0300

    added phone number validation

--- src/features/user/create-user/model/userFormSchema.js
+++ src/features/user/create-user/model/userFormSchema.js
@@ -1,10 +1,16 @@
+import { isValidPhoneNumber } from "react-phone-number-input";
 import * as yup from "yup";
 
 const userFormSchema = yup.object({
   firstName: yup.string().required("First name is required"),
   lastName: yup.string().required("Last name is required"),
   email: yup.string().required("Email is required").email("Email is invalid"),
-  phoneNumber: yup.string().required("Phone number is required"),
+  phoneNumber: yup
+    .string()
+    .required("Phone number is required")
+    .test("Phone number validation", "Phone number is invalid", (phoneNumber) =>
+      isValidPhoneNumber(phoneNumber)
+    ),
   dateOfBirth: yup
     .date()
     .min(new Date("1900-01-01"), "Date of birth is invalid")
